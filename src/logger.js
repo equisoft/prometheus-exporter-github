9569dcbc95830724b061c7b5f6a0bc36295351f8
Nicolas Vanheuverzwijn <nicolas.vanheu@gmail.com>
2019-05-18 19:38:58 -0400

    logger: add timestamp to logs

--- src/logger.js
+++ src/logger.js
@@ -3,7 +3,10 @@ const config = require('./config');
 
 module.exports = winston.createLogger({
     level: config.log.level,
-    format: winston.format[config.log.format](),
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format[config.log.format](),
+    ),
     transports: [
         new winston.transports.Console({ level: config.log.level }),
     ],
